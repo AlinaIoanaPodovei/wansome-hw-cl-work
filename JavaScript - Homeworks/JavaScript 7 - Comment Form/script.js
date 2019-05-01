baeb38e612b47874fa5c8f01567e88de8b8c727d
AlinaIoanaPodovei <pia_alina2012@yahoo.com>
2019-05-01 23:46:17 +0300

    ok

--- JavaScript - Homeworks/JavaScript 7 - Comment Form/script.js
+++ JavaScript - Homeworks/JavaScript 7 - Comment Form/script.js
@@ -8,3 +8,18 @@ function showTextarea(){
         myTextarea.style.display = 'none';
     }
 }
+
+
+document.getElementById('submit').addEventListener('click', textLimit);
+function textLimit(){
+    var text = document.getElementById('text1');
+    var error = document.getElementById('error');
+    var textArea = document.getElementById('text1').textLength;
+
+    if(textArea > 2){
+       text.style.color = 'red';
+       error.style.display = 'block';
+    }else {
+        console.log('all good here');
+    }
+}
